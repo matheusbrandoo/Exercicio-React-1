22329b77cc490dae7d584b4e069de8bcdab855c0
Matheus Brandão <mateusbrandoo@gmail.com>
2024-09-09 20:23:41 -0300

    Exercicio React 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Blog from "./Componentes/Blog"
+import Cabecalho from "./Componentes/cabecalho"
+import Rodape from "./Componentes/Rodape"
+
+const App = () => {
+    return (
+        <div>
+            <Cabecalho />
+            <Blog />
+            < Rodape />
+        </div>
+    );
+}
+
+export default App;
